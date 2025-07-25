c44c48b10fd2c164469ff23e394edce44f477fde
sgarcia <stevensantosgarcia@gmail.com>
2025-07-24 21:43:37 -0500

    Add timer

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,33 @@
 module Main where
 
 import Byline
+import Control.Concurrent (threadDelay)
 import Data.Function ((&))
+import qualified Data.Maybe
 import qualified Data.Text as T
 import Data.Text.IO (putStrLn)
+import System.IO (hFlush, stdout)
+import Task (Task (..))
+import Text.Read (readMaybe)
 import Prelude hiding (putStrLn)
 
-newTask :: MonadByline m => m T.Text
+countdown :: Int -> IO ()
+countdown 0 = putStrLn $ T.pack "\nTime's up!"
+countdown n = do
+    putStr (show n ++ " seconds remaining...\r")
+    hFlush stdout
+    threadDelay 1000000
+    countdown (n - 1)
+
+newTask :: MonadByline m => m Task
 newTask = do
     sayLn $ text (T.pack "Welcome to ") <> (text (T.pack "hask-master") & bold) <> text (T.pack "!")
     let nameQuestion = (text (T.pack "Please enter the task name.") & bold) <> text (T.pack "\n")
     name <- askUntil nameQuestion Nothing (pure . validateName)
     let timeQuestion = (text (T.pack "Please enter the estimated time to completion.") & bold) <> text (T.pack "\n")
-    time <- askUntil timeQuestion Nothing (pure . validateTimeEstimate)
-    pure $ T.pack "You estimate that " <> name <> T.pack " will take you " <> time <> T.pack "."
+    timeStr <- askUntil timeQuestion Nothing (pure . validateTimeEstimate)
+    let timeInt = Data.Maybe.fromMaybe 0 (readMaybe (T.unpack timeStr))
+    pure $ Task{_taskName = name, _taskEstTime = timeInt}
 
 validateName :: T.Text -> Either (Stylized T.Text) T.Text
 validateName input
@@ -29,5 +43,6 @@ main :: IO ()
 main = do
     result <- runBylineT newTask
     case result of
-        Just task -> putStrLn $ T.pack "Task created: " <> task
+        Just task -> do
+            countdown $ _taskEstTime task
         _ -> pure ()
--- src/Task.hs
+++ src/Task.hs
@@ -7,8 +7,6 @@ import qualified Data.Text as T
 data TaskStatus = Todo | InProgress | Paused | Done | Cancelled
 
 data Task = Task
-    { _taskId :: Int
-    , _taskName :: T.Text
-    , _taskStatus :: TaskStatus
-    , _taskEstTime :: Int -- estimated time
+    { _taskName :: T.Text
+    , _taskEstTime :: Int
     }
