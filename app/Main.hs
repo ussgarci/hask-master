a2d895104474dbe253272b14383997c3e075f5a5
sgarcia <stevensantosgarcia@gmail.com>
2025-08-01 21:54:02 -0500

    Remove byline add brick

--- app/Main.hs
+++ app/Main.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Main where
 
-import Byline
-import Control.Concurrent (threadDelay, threadDelay, ThreadId)
+import Brick
+import Control.Concurrent (ThreadId, threadDelay)
 import Control.Concurrent.MVar (MVar)
 import Data.Function ((&))
+import qualified Data.Map as Map
 import qualified Data.Maybe
 import qualified Data.Text as T
 import Data.Text.IO (putStrLn)
@@ -12,13 +16,12 @@ import Task (Task (..))
 import Text.Read (readMaybe)
 import Timer (Timer (..))
 import Prelude hiding (putStrLn)
-import qualified Data.Map as Map
 
 data TimerState = Running | Paused
 
 data AppState = AppState
-    { _tasks :: [Task],
-      _taskMap :: Map.Map String (ThreadId, MVar TimerState)
+    { _tasks :: [Task]
+    , _taskMap :: Map.Map String (ThreadId, MVar TimerState)
     }
 
 countdown :: Int -> IO ()
@@ -29,30 +32,18 @@ countdown n = do
     threadDelay 1000000
     countdown (n - 1)
 
-newTask :: MonadByline m => m Task
-newTask = do
-    sayLn $ text (T.pack "Welcome to ") <> (text (T.pack "hask-master") & bold) <> text (T.pack "!")
-    let nameQuestion = (text (T.pack "Please enter the task name.") & bold) <> text (T.pack "\n")
-    name <- askUntil nameQuestion Nothing (pure . validateName)
-    let timeQuestion = (text (T.pack "Please enter the estimated time to completion.") & bold) <> text (T.pack "\n")
-    timeStr <- askUntil timeQuestion Nothing (pure . validateTimeEstimate)
-    let timeInt = Data.Maybe.fromMaybe 0 (readMaybe (T.unpack timeStr))
-    pure $ Task{_name = name, _time = timeInt, _timer = Timer 0 0 0}
+-- newTask :: MonadByline m => m Task
+-- newTask = do
+--    sayLn $ text (T.pack "Welcome to ") <> (text (T.pack "hask-master") & bold) <> text (T.pack "!")
+--    let nameQuestion = (text (T.pack "Please enter the task name.") & bold) <> text (T.pack "\n")
+--    name <- askUntil nameQuestion Nothing (pure . validateName)
+--    let timeQuestion = (text (T.pack "Please enter the estimated time to completion.") & bold) <> text (T.pack "\n")
+--    timeStr <- askUntil timeQuestion Nothing (pure . validateTimeEstimate)
+--    let timeInt = Data.Maybe.fromMaybe 0 (readMaybe (T.unpack timeStr))
+--    pure $ Task{_name = name, _time = timeInt, _timer = Timer 0 0 0}
 
-validateName :: T.Text -> Either (Stylized T.Text) T.Text
-validateName input
-    | T.null input = Left $ text (T.pack "A task name is required.") & bg red
-    | otherwise = Right input
-
-validateTimeEstimate :: T.Text -> Either (Stylized T.Text) T.Text
-validateTimeEstimate input
-    | T.length input < 1 = Left $ text (T.pack "Estimated time must be a positive integer.") & bg red
-    | otherwise = Right input
+ui :: Widget ()
+ui = str "Hello, world!"
 
 main :: IO ()
-main = do
-    result <- runBylineT newTask
-    case result of
-        Just task -> do
-            countdown $ _time task
-        _ -> pure ()
+main = simpleMain ui
--- src/Timer.hs
+++ src/Timer.hs
@@ -1,10 +1,18 @@
 module Timer (
     Timer (..),
+    tick,
 ) where
 
 data Timer = Timer
-    { hours :: Int
-    , minutes :: Int
-    , seconds :: Int
+    { _hours :: Int
+    , _minutes :: Int
+    , _seconds :: Int
     }
     deriving (Show, Eq, Ord)
+
+tick :: Timer -> Maybe Timer
+tick (Timer h m s)
+    | s > 0 = Just (Timer h m (s - 1))
+    | m > 0 = Just (Timer h (m - 1) 59)
+    | h > 0 = Just (Timer (h - 1) 59 59)
+    | otherwise = Nothing
